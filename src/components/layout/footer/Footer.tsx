6ca2443b625d8f4bf51e9214b8db1b84f0806cae
Leonardo <leovpc@gmail.com>
2023-10-31 00:10:02 -0600

    added whiteListPaths for show or not the component

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -3,12 +3,13 @@ import { AddButton } from "./AddButton";
 import { usePathname } from "next/navigation";
 import { LinksFooter } from "./LinksFooter";
 import { LinksItemFooter } from "./LinksItemFooter";
+import { whiteListPaths } from "@/data/constants";
 
 export const Footer = () => {
     const path = usePathname();
     return (
         <>
-            {path !== "/sesion-de-usuario" && (
+            {!whiteListPaths.includes(path) && (
                 <footer className="fixed z-10 bottom-0 h-16 w-full bg-primario ">
                     <div className="flex gap-28 justify-center items-center w-full h-full  ">
                         <AddButton />
