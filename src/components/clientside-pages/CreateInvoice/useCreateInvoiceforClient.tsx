586f1989022abf767d2294feaa22ef4b85274062
Leonardo <leovpc@gmail.com>
2023-11-06 22:05:59 -0600

    added new hook for handle Create Invoices Details

--- src/components/clientside-pages/CreateInvoice/useCreateInvoiceforClient.tsx
+++ src/components/clientside-pages/CreateInvoice/useCreateInvoiceforClient.tsx
@@ -1,38 +1,46 @@
 import { handleOnChange } from "@/components/Utils/formUtils";
 import { getCurrentDate } from "@/components/Utils/getCurrentDate";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import { fetchAPI } from "../../Utils/fetchAPI";
 import { useQuery } from "@tanstack/react-query";
 import { useStore } from "@nanostores/react";
 import { $selectedBusiness } from "@/stores/business";
-import { EditRowIcon } from "@/icons/EditRowIcon";
-import { DeleteRowIcon } from "@/icons/DeleteRowIcon";
-import toast from "react-hot-toast";
-import { moneyFormat } from "@/components/Utils/MoneyFormat";
-import { Tooltip, useDisclosure } from "@nextui-org/react";
+import { useInvoiceDetail } from "./useInvoiceDetail";
 
 export const useCreateInvoiceforClient = ({ id }: { id: string }) => {
+    // Hook para manejar los detalles de la factura
+    const {
+        invoiceDetails,
+        formDataDetail,
+        handleOnChangeDetail,
+        handleAddInvoiceDetail,
+        handleOpenAddDetail,
+        isOpen,
+        onOpenChange,
+        codeInput,
+        renderCell,
+        columnNames,
+        handleCloseModal,
+    } = useInvoiceDetail({ id });
+    // estado para almacenar los datos del cliente
+    const [client, setClient] = useState({ username: "", active: 0 });
+    // estado para almacenar el negocio seleccionado
     const selectedBusiness = useStore($selectedBusiness);
+    // estado para almacenar el negocio seleccionado con el nombre y si el cliente tiene balances en ese negocio
     const [businessSelected, setBusinessSelected] =
         useState<BusinessSelecterProps>({
             id: 0,
             name: "",
             isClientInBusiness: true,
         });
-    const initialStateInvoiceDetail: InitialStateInvoiceDetailProps = {
-        code: "",
-        quantity: 0,
-        price: 0,
-        description: "",
-    };
-    const [invoiceDetails, setInvoiceDetails] = useState([
-        initialStateInvoiceDetail,
-    ]);
+
+    // estado para almacenar los datos de la factura
     const [formInvoice, setFormInvoice] = useState<FormValuesNewInvoice>({
         client_id: parseInt(id, 10),
         date: getCurrentDate(),
     });
-    const [client, setClient] = useState({ username: "", active: 0 });
+
+    // obtener los datos del cliente
     const { status: statusFetchClient, data: clientData } = useQuery({
         queryKey: ["fetch-client"],
         queryFn: async () =>
@@ -41,11 +49,8 @@ export const useCreateInvoiceforClient = ({ id }: { id: string }) => {
             }),
         retry: 2,
     });
-    const {
-        status: statusBusiness,
-        data: dataBusiness,
-        isLoading: isLoadingBusiness,
-    } = useQuery({
+    // obtener los negocios del cliente que esta logueado
+    const { status: statusBusiness, data: dataBusiness } = useQuery({
         queryKey: ["users-business"],
         queryFn: async () =>
             await fetchAPI({
@@ -53,6 +58,7 @@ export const useCreateInvoiceforClient = ({ id }: { id: string }) => {
             }),
         retry: 2,
     });
+    // obtener el balance del cliente en el negocio seleccionado
     const { status: statusBalance, data: dataBalance } = useQuery({
         queryKey: ["user-balance"],
         queryFn: async () =>
@@ -62,6 +68,7 @@ export const useCreateInvoiceforClient = ({ id }: { id: string }) => {
         retry: 2,
     });
     useEffect(() => {
+        // si el cliente existe, setearlo en el estado
         if (statusFetchClient === "success") {
             setClient(clientData);
         } else if (statusFetchClient === "error") {
@@ -75,12 +82,15 @@ export const useCreateInvoiceforClient = ({ id }: { id: string }) => {
             statusBusiness === "success" &&
             selectedBusiness
         ) {
+            // buscar el nombre del negocio seleccionado
             const findBusinessName = dataBusiness.find(
                 (item: BusinessProps) => item.id === selectedBusiness
             );
+            // buscar los negocios donde el cliente tiene balances
             const businessofClient = dataBalance.map(
                 (item: any) => item.business_id
             );
+            // setear el negocio seleccionado con el nombre y si el cliente tiene balances en ese negocio
             setBusinessSelected({
                 id: findBusinessName?.id,
                 name: findBusinessName?.name,
@@ -97,184 +107,6 @@ export const useCreateInvoiceforClient = ({ id }: { id: string }) => {
         statusFetchClient,
     ]);
 
-    const { isOpen, onOpen, onOpenChange } = useDisclosure();
-
-    const codeInput: React.RefObject<HTMLInputElement> = useRef(null);
-    // Estado para almacenar los detalles de la factura
-
-    const [isEditing, setIsEditing] = useState<{
-        state: boolean;
-        index: number | null;
-    }>({ state: false, index: null });
-    const [formDataDetail, setFormDataDetail] = useState(
-        initialStateInvoiceDetail
-    );
-    //Estado para almacenar las columnas de la tabla
-    const columnNames: ColumnNamesProps[] = [
-        { key: "code", name: "Codigo" },
-        { key: "quantity", name: "Cantidad" },
-        { key: "price", name: "Precio" },
-        { key: "description", name: "Descripcion" },
-        { key: "subtotal", name: "Subtotal" },
-        { key: "actions", name: "Acciones" },
-    ];
-    const handleOpenAddDetail = () => {
-        setFormDataDetail(initialStateInvoiceDetail);
-        onOpen();
-    };
-    // Función para agregar un nuevo detalle de factura al estado
-    const handleAddInvoiceDetail: HandleAddInvoiceDetailProps = (onClose) => {
-        if (
-            formDataDetail.quantity === 0 ||
-            formDataDetail.price === 0 ||
-            formDataDetail.description === ""
-        ) {
-            toast.error("Por favor complete todos los campos", {
-                duration: 4000,
-            });
-            focusFirstInput();
-            return;
-        }
-        if (!isEditing.state) {
-            const idClient = localStorage.getItem("idClient");
-
-            setInvoiceDetails([...invoiceDetails, formDataDetail]);
-            if (idClient && idClient !== id) {
-                localStorage.setItem("idClient", `${id}`);
-            }
-            localStorage.setItem(
-                "invoiceDetails",
-                JSON.stringify([...invoiceDetails, formDataDetail])
-            );
-            toast.success("Detalle agregado correctamente", { duration: 4000 });
-            setFormDataDetail(initialStateInvoiceDetail);
-            focusFirstInput();
-        } else if (isEditing.state && isEditing.index !== null) {
-            const idClient = localStorage.getItem("idClient");
-            const newInvoiceDetails = [...invoiceDetails];
-            newInvoiceDetails[isEditing.index] = formDataDetail;
-            setInvoiceDetails(newInvoiceDetails);
-            localStorage.setItem(
-                "invoiceDetails",
-                JSON.stringify(newInvoiceDetails)
-            );
-            toast.success("Detalle editado correctamente", { duration: 4000 });
-            if (idClient && idClient !== id) {
-                localStorage.setItem("idClient", `${id}`);
-            }
-            setFormDataDetail(initialStateInvoiceDetail);
-            setIsEditing({ state: false, index: null });
-            onClose();
-        }
-    };
-    const focusFirstInput = () => {
-        if (codeInput.current) {
-            codeInput.current.focus();
-        }
-    };
-
-    useEffect(() => {
-        const data = localStorage.getItem("invoiceDetails");
-        const idClient = localStorage.getItem("idClient");
-
-        if (idClient === id && data) {
-            setInvoiceDetails(JSON.parse(data));
-        } else {
-            setInvoiceDetails([]);
-        }
-    }, [id]);
-    const handleOnChangeDetail: HandleOnChangeDetailProps = (e) => {
-        const { name, value } = e.target;
-
-        setFormDataDetail({ ...formDataDetail, [name]: value });
-    };
-    // Función para editar una línea de detalle
-    const handleEditInvoiceDetail = (e: any, index: any) => {
-        e.preventDefault();
-        setFormDataDetail(invoiceDetails[index]);
-        setIsEditing({ state: true, index });
-        onOpen();
-    };
-    const handleRemoveInvoiceDetail = (e: any, index: any) => {
-        e.preventDefault();
-        const idClient = localStorage.getItem("idClient");
-        if (idClient && idClient !== id) {
-            localStorage.setItem("idClient", `${id}`);
-        }
-        setInvoiceDetails(
-            invoiceDetails.filter(
-                (detail, detailIndex) => detailIndex !== index
-            )
-        );
-        localStorage.setItem(
-            "invoiceDetails",
-            JSON.stringify(
-                invoiceDetails.filter(
-                    (detail, detailIndex) => detailIndex !== index
-                )
-            )
-        );
-    };
-    const handleCloseModal = (onClose: any) => {
-        setFormDataDetail(initialStateInvoiceDetail);
-        if (isEditing) {
-            setIsEditing({ state: false, index: 0 });
-        }
-        onClose();
-    };
-    const renderCell: RenderCellProps = (detail, columnKey, index) => {
-        const cellValue =
-            detail[columnKey as keyof InitialStateInvoiceDetailProps];
-
-        switch (columnKey) {
-            case "code":
-                return <p>{detail.code}</p>;
-            case "quantity":
-                return <p>{detail.quantity}</p>;
-            case "price":
-                return <p>{moneyFormat(detail.price, "CRC", "es-CR")}</p>;
-            case "description":
-                return <p>{detail.description}</p>;
-            case "subtotal":
-                return (
-                    <p>
-                        {moneyFormat(
-                            detail.quantity * detail.price,
-                            "CRC",
-                            "es-CR"
-                        )}
-                    </p>
-                );
-
-            case "actions":
-                return (
-                    <div className="relative flex items-center gap-2">
-                        <Tooltip content="Editar detalle">
-                            <button
-                                onClick={(e) =>
-                                    handleEditInvoiceDetail(e, index)
-                                }
-                                className="text-lg text-default-400 cursor-pointer active:opacity-50"
-                            >
-                                <EditRowIcon />
-                            </button>
-                        </Tooltip>
-                        <Tooltip color="danger" content="Eliminar detalle">
-                            <button
-                                onClick={(e) =>
-                                    handleRemoveInvoiceDetail(e, index)
-                                }
-                                className="text-lg text-danger cursor-pointer active:opacity-50"
-                            >
-                                <DeleteRowIcon />
-                            </button>
-                        </Tooltip>
-                    </div>
-                );
-            default:
-                return <p>Columna Invalida</p>;
-        }
-    };
     return {
         ...formInvoice,
         ...client,
---
+++ src/components/clientside-pages/CreateInvoice/useInvoiceDetail.tsx
@@ -0,0 +1,224 @@
+import { EditRowIcon } from "@/icons/EditRowIcon";
+import { DeleteRowIcon } from "@/icons/DeleteRowIcon";
+import toast from "react-hot-toast";
+import { moneyFormat } from "@/components/Utils/MoneyFormat";
+import { Tooltip, useDisclosure } from "@nextui-org/react";
+import { useEffect, useState, useRef } from "react";
+
+export const useInvoiceDetail = ({ id }: { id: string }) => {
+    // Datos iniciales para los detalles de la factura
+    const initialStateInvoiceDetail: InitialStateInvoiceDetailProps = {
+        code: "",
+        quantity: 0,
+        price: 0,
+        description: "",
+    };
+    // Estado para almacenar los detalles de la factura
+    const [invoiceDetails, setInvoiceDetails] = useState([
+        initialStateInvoiceDetail,
+    ]);
+    // Hook para manejar el modal de agregar detalle
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
+    // Referencia para el input de código en el modal de agregar detalle
+    const codeInput: React.RefObject<HTMLInputElement> = useRef(null);
+
+    // Estado para almacenar si se esta editando un detalle
+    const [isEditing, setIsEditing] = useState<{
+        state: boolean;
+        index: number | null;
+    }>({ state: false, index: null });
+    // Estado para almacenar los datos del detalle de la factura que se esta agregando
+    const [formDataDetail, setFormDataDetail] = useState(
+        initialStateInvoiceDetail
+    );
+    //Estado para almacenar las columnas de la tabla
+    const columnNames: ColumnNamesProps[] = [
+        { key: "code", name: "Codigo" },
+        { key: "quantity", name: "Cantidad" },
+        { key: "price", name: "Precio" },
+        { key: "description", name: "Descripcion" },
+        { key: "subtotal", name: "Subtotal" },
+        { key: "actions", name: "Acciones" },
+    ];
+    // Función para abrir el modal de agregar detalle
+    const handleOpenAddDetail = () => {
+        setFormDataDetail(initialStateInvoiceDetail);
+        onOpen();
+    };
+    // Función para agregar un nuevo detalle de factura al estado
+    const handleAddInvoiceDetail: HandleAddInvoiceDetailProps = (onClose) => {
+        if (
+            formDataDetail.quantity === 0 ||
+            formDataDetail.price === 0 ||
+            formDataDetail.description === ""
+        ) {
+            toast.error("Por favor complete todos los campos", {
+                duration: 4000,
+            });
+            focusFirstInput();
+            return;
+        }
+        // Si no se esta editando un detalle se agrega uno nuevo al estado de detalles de factura y al localstorage
+        if (!isEditing.state) {
+            const idClient = localStorage.getItem("idClient");
+
+            setInvoiceDetails([...invoiceDetails, formDataDetail]);
+            if (idClient && idClient !== id) {
+                localStorage.setItem("idClient", `${id}`);
+            }
+            localStorage.setItem(
+                "invoiceDetails",
+                JSON.stringify([...invoiceDetails, formDataDetail])
+            );
+            toast.success("Detalle agregado correctamente", { duration: 4000 });
+            setFormDataDetail(initialStateInvoiceDetail);
+            focusFirstInput();
+            // Si se esta editando un detalle se edita el detalle en el estado de detalles de factura y en el localstorage
+        } else if (isEditing.state && isEditing.index !== null) {
+            const idClient = localStorage.getItem("idClient");
+            const newInvoiceDetails = [...invoiceDetails];
+            newInvoiceDetails[isEditing.index] = formDataDetail;
+            setInvoiceDetails(newInvoiceDetails);
+            localStorage.setItem(
+                "invoiceDetails",
+                JSON.stringify(newInvoiceDetails)
+            );
+            toast.success("Detalle editado correctamente", { duration: 4000 });
+            if (idClient && idClient !== id) {
+                localStorage.setItem("idClient", `${id}`);
+            }
+            setFormDataDetail(initialStateInvoiceDetail);
+            setIsEditing({ state: false, index: null });
+            onClose();
+        }
+    };
+    // Función para enfocar el primer input del modal de agregar detalle
+    const focusFirstInput = () => {
+        if (codeInput.current) {
+            codeInput.current.focus();
+        }
+    };
+    // Obtener los detalles de la factura del localstorage
+    useEffect(() => {
+        const data = localStorage.getItem("invoiceDetails");
+        const idClient = localStorage.getItem("idClient");
+
+        if (idClient === id && data) {
+            setInvoiceDetails(JSON.parse(data));
+        } else {
+            setInvoiceDetails([]);
+        }
+    }, [id]);
+
+    // Función para manejar el cambio de los inputs del modal de agregar detalle
+    const handleOnChangeDetail: HandleOnChangeDetailProps = (e) => {
+        const { name, value } = e.target;
+
+        setFormDataDetail({ ...formDataDetail, [name]: value });
+    };
+    // Función para editar una línea de detalle
+    const handleEditInvoiceDetail = (e: any, index: any) => {
+        e.preventDefault();
+        setFormDataDetail(invoiceDetails[index]);
+        setIsEditing({ state: true, index });
+        onOpen();
+    };
+    // Función para eliminar una línea de detalle
+    const handleRemoveInvoiceDetail = (e: any, index: any) => {
+        e.preventDefault();
+        const idClient = localStorage.getItem("idClient");
+        // Si el id del cliente en el localstorage es diferente al id del cliente actual se elimina el id del cliente del localstorage
+        if (idClient && idClient !== id) {
+            localStorage.setItem("idClient", `${id}`);
+        }
+        // Se elimina la línea de detalle del estado y del localstorage
+        setInvoiceDetails(
+            invoiceDetails.filter(
+                (detail, detailIndex) => detailIndex !== index
+            )
+        );
+        localStorage.setItem(
+            "invoiceDetails",
+            JSON.stringify(
+                invoiceDetails.filter(
+                    (detail, detailIndex) => detailIndex !== index
+                )
+            )
+        );
+    };
+    // Función para cerrar el modal de agregar detalle
+    const handleCloseModal = (onClose: any) => {
+        setFormDataDetail(initialStateInvoiceDetail);
+        if (isEditing) {
+            setIsEditing({ state: false, index: 0 });
+        }
+        onClose();
+    };
+    // Función para renderizar las celdas de la tabla de detalles de la factura
+    const renderCell: RenderCellProps = (detail, columnKey, index) => {
+        const cellValue =
+            detail[columnKey as keyof InitialStateInvoiceDetailProps];
+
+        switch (columnKey) {
+            case "code":
+                return <p>{detail.code}</p>;
+            case "quantity":
+                return <p>{detail.quantity}</p>;
+            case "price":
+                return <p>{moneyFormat(detail.price, "CRC", "es-CR")}</p>;
+            case "description":
+                return <p>{detail.description}</p>;
+            case "subtotal":
+                return (
+                    <p>
+                        {moneyFormat(
+                            detail.quantity * detail.price,
+                            "CRC",
+                            "es-CR"
+                        )}
+                    </p>
+                );
+
+            case "actions":
+                return (
+                    <div className="relative flex items-center gap-2">
+                        <Tooltip content="Editar detalle">
+                            <button
+                                onClick={(e) =>
+                                    handleEditInvoiceDetail(e, index)
+                                }
+                                className="text-lg text-default-400 cursor-pointer active:opacity-50"
+                            >
+                                <EditRowIcon />
+                            </button>
+                        </Tooltip>
+                        <Tooltip color="danger" content="Eliminar detalle">
+                            <button
+                                onClick={(e) =>
+                                    handleRemoveInvoiceDetail(e, index)
+                                }
+                                className="text-lg text-danger cursor-pointer active:opacity-50"
+                            >
+                                <DeleteRowIcon />
+                            </button>
+                        </Tooltip>
+                    </div>
+                );
+            default:
+                return <p>Columna Invalida</p>;
+        }
+    };
+    return {
+        invoiceDetails,
+        formDataDetail,
+        handleOnChangeDetail,
+        handleAddInvoiceDetail,
+        handleOpenAddDetail,
+        isOpen,
+        onOpenChange,
+        codeInput,
+        renderCell,
+        columnNames,
+        handleCloseModal,
+    };
+};
