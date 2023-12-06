c2024259c334a05ccaafff8b74cce891a82e55c2
Leonardo <leovpc@gmail.com>
2023-12-06 00:56:54 -0600

    Add cellphone and country fields to User interface

--- src/stores/users.ts
+++ src/stores/users.ts
@@ -3,66 +3,58 @@ interface User {
   id: number;
   username: string;
   email: string;
+  cellphone: string;
   active: boolean;
+  country: string;
   createdAt: string;
   updatedAt: string;
 }
-
+interface Balance {
+  amount: string;
+}
 interface UserMessage {
   message: string;
 }
 
-interface ApiResponse {
+interface UserData {
   isLoggedIn: boolean;
   user: User;
   roles: number[];
-  client: Client;
+  client: Client[];
   balance: number;
   userMessage: UserMessage;
 }
 interface Client {
   id: number;
-  username: string;
-  email: string;
-  cellphone: string;
-  token: string;
-  pin: null | string;
-  active: boolean;
-  country: string;
-  detail: null | string;
-  createdAt: string;
-  updatedAt: string;
-  user_id: number;
   parent_user_id: number;
+  balance: Balance;
 }
 
-export type IsLoggedInValue = ApiResponse;
+export type IsLoggedInValue = UserData;
 export const $user = atom<IsLoggedInValue>({
-  isLoggedIn: true,
+  isLoggedIn: false,
   user: {
     id: 0,
     username: "",
     email: "",
+    cellphone: "",
     active: false,
+    country: "",
     createdAt: "",
     updatedAt: "",
   },
   roles: [],
-  client: {
-    id: 0,
-    username: "",
-    email: "",
-    cellphone: "",
-    token: "",
-    pin: "",
-    active: false,
-    country: "",
-    detail: "",
-    createdAt: "",
-    updatedAt: "",
-    user_id: 0,
-    parent_user_id: 0,
-  },
+  client: [
+    {
+      id: 0,
+      parent_user_id: 0,
+      balance: {
+        amount: "0.00",
+      },
+    },
+  ],
   balance: 0,
-  userMessage: { message: "" },
+  userMessage: {
+    message: "",
+  },
 });
